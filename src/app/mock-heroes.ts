212fdabf21438628e8aa67f6a030bb7dc6d24d2b
juliaanholcer <julia.anholcer@interia.pl>
2017-11-16 08:51:52 +0100

    Initial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10, name: 'Zorya Vechernyaya'},
+  { id: 11, name: 'Shadow Moon' },
+  { id: 12, name: 'Mr. Wednesday' },
+  { id: 13, name: 'Bilquis' },
+  { id: 14, name: 'Belabog' },
+  { id: 15, name: 'Czernobog' },
+  { id: 16, name: 'Mr. World' },
+  { id: 17, name: 'Technical Boy' },
+  { id: 18, name: 'Mr. Ibis' },
+  { id: 19, name: 'Anubis' },
+  { id: 20, name: 'Mr. Nancy' }
+];
